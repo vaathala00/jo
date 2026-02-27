1ca52956d2eff6b42be65ad33cd92d0c25b55e57
vaathala00 <165231338+vaathala00@users.noreply.github.com>
2026-02-27 17:58:36 +0530

    Files upload

--- scraper/star not workingindex.js
+++ scraper/star not workingindex.js
@@ -50,12 +50,7 @@ async function fetchAndSaveJson() {
       // Extract URL after license
       else if (currentKid && currentKey && currentTvgId && trimmed.startsWith("http")) {
         // Remove extra &xxx=... if present
-        let cleanUrl = trimmed.split("&xxx=")[0];
-
-        // --- MODIFICATION START ---
-        // Replace the old domain with the new domain
-        cleanUrl = cleanUrl.replace("jiotvmblive.cdn.jio.com", "jiotvbpkmob.cdn.jio.com");
-        // --- MODIFICATION END ---
+        const cleanUrl = trimmed.split("&xxx=")[0];
 
         result[currentTvgId] = {
           kid: currentKid,
